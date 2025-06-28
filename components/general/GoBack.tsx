b4305d4396e09ff19341dfbe5675cf8bfea4d3b5
tomiwa-code <ogunbaseayoola@gmail.com>
2025-06-28 21:24:08 +0100

    Feat: Added home button

--- components/general/GoBack.tsx
+++ components/general/GoBack.tsx
@@ -3,13 +3,14 @@ import React from "react";
 
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Home } from "lucide-react";
+import Link from "next/link";
 
 const GoBack = () => {
   const router = useRouter();
 
   return (
-    <div className="absolute md:fixed top-8 md:top-10 left-5 md:left-20">
+    <div className="absolute md:fixed top-8 md:top-10 left-5 md:left-20 flex items-center gap-x-4">
       <Button
         variant={"outline"}
         className="bg-transparent text-white"
@@ -17,6 +18,12 @@ const GoBack = () => {
       >
         <ArrowLeft />
       </Button>
+
+      <Link href={"/"}>
+        <Button variant={"outline"} className="bg-transparent text-white">
+          <Home />
+        </Button>
+      </Link>
     </div>
   );
 };
